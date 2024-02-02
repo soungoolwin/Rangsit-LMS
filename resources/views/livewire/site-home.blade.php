58fe72953e49558f2d94f5bc0935e067bce730b7
Soung Oo Lwin <soungoolwin275@gmail.com>
2024-02-02 19:17:47 +0700

    Navigation Fix

--- resources/views/livewire/site-home.blade.php
+++ resources/views/livewire/site-home.blade.php
@@ -1,11 +1,60 @@
 <div>
     <div x-data="{ sidebarOpen: false }" class="flex overflow-x-hidden h-screen">
-        <aside class="flex-shrink-0 w-64 flex flex-col border-r transition-all duration-300 bg-[#263238]"
-            :class="{ '-ml-48': !sidebarOpen }">
+        <!-- Fixed top navigation -->
+        <nav class="fixed top-0 left-0 w-full bg-white text-black p-4 z-50">
+            <div class="flex justify-between items-center">
+                <div> <img src="{{ Vite::image('rsulogo.png') }}" alt="" class="w-[160px] h-[60px]"
+                        :class="{ 'ml-64': sidebarOpen }">
+                </div>
+                <div class="flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6 mr-5">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+                    </svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6 mr-5">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                    </svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6 mr-5">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
+                    </svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6 mr-5">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+
+
+                    <button @click="sidebarOpen = !sidebarOpen">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                    </button>
+                </div>
+
+
+
+            </div>
+        </nav>
+
+        <!-- Fixed sidebar -->
+        <aside x-show="sidebarOpen"
+            class="top-0 left-0 h-full w-64 bg-[#263238] text-white transition-transform duration-300 transform translate-x-0"
+            :class="{ '-translate-x-full': !sidebarOpen, 'fixed': sidebarOpen, 'absolute': !sidebarOpen, 'z-50': sidebarOpen }">
+            <!-- Sidebar content -->
             <div class="h-screen">
-                <nav class="flex-1 flex flex-col relative" :class="{ 'ml-6': !sidebarOpen, 'md:ml-0': sidebarOpen }">
-                    <a href="#" class="p-2 flex w-[90%] mx-auto ml-6 mt-[40px]" @click="sidebarOpen = !sidebarOpen"
-                        @click="sidebarOpen = !sidebarOpen">
+                <nav class="flex-1 flex flex-col" :class="{ 'ml-6 relative': !sidebarOpen, 'md:ml-0': sidebarOpen }">
+                    <a href="#" class="p-2 flex w-[90%] mx-auto ml-6 mt-[40px]"
+                        @click="sidebarOpen = !sidebarOpen" @click="sidebarOpen = !sidebarOpen">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6 text-white mr-5"
                             :class="{ 'absolute right-1': !sidebarOpen, 'md:relative': sidebarOpen }">
@@ -42,8 +91,8 @@
                         <span class="text-white">Calendar</span>
                     </a>
                     <a href="#" class="p-2 flex w-[90%] mx-auto ml-6 mt-[20px]">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6 text-white mr-5"
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white mr-5"
                             :class="{ 'absolute right-1': !sidebarOpen, 'md:relative': sidebarOpen }">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
@@ -51,8 +100,8 @@
                         <span class="text-white">Certificates</span>
                     </a>
                     <a href="#" class="p-2 flex w-[90%] mx-auto ml-6 mt-[20px]">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6 text-white mr-5"
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white mr-5"
                             :class="{ 'absolute right-1': !sidebarOpen, 'md:relative': sidebarOpen }">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
@@ -60,8 +109,8 @@
                         <span class="text-white">Private Files</span>
                     </a>
                     <a href="#" class="p-2 flex w-[90%] mx-auto ml-6 mt-[20px]">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6 text-white mr-5"
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white mr-5"
                             :class="{ 'absolute right-1': !sidebarOpen, 'md:relative': sidebarOpen }">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
@@ -70,43 +119,20 @@
                     </a>
                 </nav>
             </div>
-
         </aside>
-        <div class="flex-1">
-            {{-- <header class="flex items-center p-4 text-semibold text-gray-100 bg-white">
-                <img src="{{ Vite::image('rsulogo.png') }}" alt="" class="h-15">
-            </header> --}}
-            <nav class="grid grid-cols-1 lg:grid-cols-2 gap-3 mx-4">
-                <div class="lg:block hidden">
-                    <img src="{{ Vite::image('rsulogo.png') }}" class="" alt="">
-                </div>
-                <div class="ml-auto mr-[50px] my-[15px] flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6 mr-6 text-gray">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6 mr-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6 mr-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6 mr-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    </svg>
 
-                </div>
-            </nav>
-
-            <main class="p-4 bg-[#F8F9FA] h-screen">
-                Main Content
+        <!-- Main content -->
+        <div class="flex-1 transition-margin duration-300 mt-[90px]"
+            :class="{ 'ml-0': !sidebarOpen, 'ml-64': sidebarOpen }">
+            <main class="p-4 bg-[#F8F9FA]">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex consequuntur eaque, sapiente culpa
+                    fugiat, quis illo unde nostrum, laboriosam accusantium quaerat beatae amet eligendi quod in aperiam
+                    ea. Necessitatibus est quisquam repellat saepe? Neque cupiditate quibusdam, unde vero deserunt optio
+                    quis assumenda eveniet nostrum quasi magni perspiciatis fuga accusamus. Tenetur doloremque adipisci,
+                    facilis aperiam quam eos quod nesciunt eligendi autem aspernatur minus deserunt hic molestias
+                    reiciendis nam soluta ipsa beatae culpa quos! Quaerat possimus voluptas mollitia error ipsam
+                    consectetur quis odio omnis consequuntur debitis eum ipsa expedita vero deleniti, ratione in aliquam
+                    quia accusantium, esse quidem culpa, eaque dolor numquam.</p>
             </main>
         </div>
     </div>
