e186c6f3899a6e6d6e73fc5fa8a766f601bf5f71
yamone178 <yamonemyatmoe1592@gmail.com>
2024-02-24 20:13:49 +0630

    create-migration for course,category and subCat

---
+++ app/Models/Subcategory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Subcategory extends Model
+{
+    use HasFactory;
+
+    public function category():BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function courses():HasMany
+    {
+        return $this->hasMany(Course::class);
+    }
+}
---
+++ database/migrations/2024_02_24_075114_create_courses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Subcategory;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('course_name');
+            $table->enum('visibility', ['public', 'private'])->default('private');
+            $table->foreignIdFor(Category::class)->nullable()->constrained();
+            $table->foreignIdFor(Subcategory::class)->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
